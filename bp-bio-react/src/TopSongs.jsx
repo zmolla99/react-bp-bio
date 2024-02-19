03d20cadd5439ee4de7ed1f89fd9572cc8129dc7
zephania <zephania1656@live.com>
2024-02-19 14:57:46 +0000

    add content to top songs component

--- bp-bio-react/src/TopSongs.jsx
+++ bp-bio-react/src/TopSongs.jsx
@@ -5,6 +5,11 @@ const TopSongs = () => {
     return (
         <>
             <h1>Top songs:</h1>
+            <ol>
+                <li><h3>How You Like That</h3> <img className='cover' src='./src/assets/song1.png' alt='song 1' /> <button>Like</button> </li>
+                <li><h3>Pink Venom</h3> <img className='cover' src='./src/assets/song2.jpg' alt='song 1' /> <button>Like</button> </li>
+                <li><h3>Shut Down</h3> <img className='cover' src='./src/assets/song3.jpg' alt='song 1' /> <button>Like</button> </li>
+            </ol>
         </>
     )
 }
